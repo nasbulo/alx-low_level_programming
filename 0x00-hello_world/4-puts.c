467018e5ab6663eaee197e34ba9bd9edbd27faec
nasbulo <nasifbl@gmail.com>
2023-02-16 00:39:29 -0800

    AS

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 /**
-* main  - A day of days
-* Return: 0 (success)
-*/
-int main(viod)
+ * main - A schedule is transfer for nex tweek
+ * Return: 0 (Success)
+ */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
+
+
+
+
+
+
+
+
+
 
