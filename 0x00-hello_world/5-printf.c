8f3a43ef883c6d89c309bc59a88f06923e3a238c
nasbulo <nasifbl@gmail.com>
2023-02-16 01:14:40 -0800

    EE

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- * main - A next scedule is transfer to years
- * Return: 0 (Success)
+ * main - A next schudule is transfered
+ * Return: 0 (success)
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n")
 return (0);
-}
