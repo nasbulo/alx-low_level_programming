66e8ae0d5fe93b07f759fa3685e57a1cdd1a5578
nasbulo <nasifbl@gmail.com>
2023-02-22 03:03:00 -0800

    YT

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
- *__isalpha - Return 1 if c is a letter. lowercase or uppercase
+ *_isalpha - Return 1 if c is a letter. lowercase or uppercase
  *
  *@c: The int to print
  *Return: Always 0.
  */
-
 int _isalpha(int c)
 
 {
