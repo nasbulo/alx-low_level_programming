f22845d7d36915cd2eea4c7a62df1f23048f3d85
nasbulo <nasifbl@gmail.com>
2023-02-22 04:17:50 -0800

    L

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -14,9 +14,9 @@ int main(void)
 	printf("%lu", j);
 	for (i = 1; i < 91; i++)
 	{
-	printf(", %lu", k);
-	k = k + j;
-	j = k - j;
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
 	}
 	j1 = j / 1000000000;
 	j2 = j % 1000000000;
