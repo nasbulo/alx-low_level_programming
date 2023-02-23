ed35940b145b0b1f77204324a147e436f3ef15bc
nasbulo <nasifbl@gmail.com>
2023-02-23 03:02:52 -0800

    OP

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,9 +2,9 @@
 
 /**
  * print_numbers - Print the numbers since 0 up to 9
- *
  * Return: The numbers since 0 up to 9
  */
+
 void print_numbers(void)
 {
 	int x;
@@ -13,6 +13,5 @@ void print_numbers(void)
 	{
 		_putchar(x + '0');
 	}
-
 	_putchar('\n');
 }
