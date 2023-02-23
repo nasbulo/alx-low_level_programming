6074d59b6a459deb955d19505d047f23b69f2da6
nasbulo <nasifbl@gmail.com>
2023-02-22 23:58:35 -0800

    ORO

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,10 +5,14 @@
  * @x:The number to be checked
  * Return: 1 for upper letter or 0 for any else
  */
+
 int _isupper(int c)
+
 {
 	if ((c >= 'A') && (c <= 'Z'))
+	{
 		return (1);
+	}
 	else
 		return (1);
 }
